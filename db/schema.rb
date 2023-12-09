d7edfc1e062f389307cf627f18287f36e00ccaf7
Miguel Perdomo <miguelferneyp@gmail.com>
2023-12-09 11:40:41 -0500

    Add book model, helper, views, and tests

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_09_003810) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_09_012423) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.string "author"
+    t.text "description"
+    t.string "isbn"
+    t.integer "pages_count"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "pets", force: :cascade do |t|
     t.string "name"
